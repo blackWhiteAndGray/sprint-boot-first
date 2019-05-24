c028e7202918072dd5d27b6fc643c96971122a9c
pangfeng <coldpang.top@gmail.com>
2019-05-24 09:18:51 +0800

    first commit.

---
+++ src/main/java/com/pangfeng/sprintbootfirst/SprintBootFirstApplication.java
@@ -0,0 +1,48 @@
+package com.pangfeng.sprintbootfirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+@SpringBootApplication
+@Service
+public class SprintBootFirstApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintBootFirstApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SprintBootFirstApplication.class);
+	}
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "hello world!";
+	}
+
+	@RequestMapping("/index")
+	public String index() {
+		return "index";
+	}
+
+	@RequestMapping("/rest")
+	@ResponseBody
+	public Map<String, Object> rest() {
+		Map<String, Object> data = new HashMap<>();
+		data.put("1", "A");
+		data.put("2", 2);
+		return data;
+	}
+}
