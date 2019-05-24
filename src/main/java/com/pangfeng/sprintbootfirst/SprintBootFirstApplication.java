00dd7f064b175d0610e65449b87aae7e4806998a
pangfeng <coldpang.top@gmail.com>
2019-05-24 16:25:27 +0800

    test push code.

--- src/main/java/com/pangfeng/sprintbootfirst/SprintBootFirstApplication.java
+++ src/main/java/com/pangfeng/sprintbootfirst/SprintBootFirstApplication.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.time.LocalDate;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
